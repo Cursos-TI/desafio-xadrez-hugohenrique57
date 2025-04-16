15082383d3e8d829f2ff92bd8fd0ccd7efc93a8c
hugo  henrique da silva ferreira <204108947+hugohenrique57@users.noreply.github.com>
2025-04-16 20:29:19 +0000

    new commit

--- xadrez.c
+++ xadrez.c
@@ -4,37 +4,52 @@
 int main() {
    
     //torre = 5 casas linha reta   bispo= 5 casas diagonal direita   rainha = casas qualquer direçao
-    int torre, bispo = 1, rainha = 1;
+    int torre, bispo = 1, rainha = 1, cavalo = 1;
 
+
+//cavalo
+    printf("##cavalo##\n");
+
+    while (cavalo <= 1)
+    {
+        int v = 1;
+        for(v = 1; v <= 2; v++){
+            printf("baixo\t");
+        }
+    printf("esquerda\n")  ;
+    cavalo++;
+    }
+
+    printf("\n");    
 //tore
     printf("##torre##\n");
     for(torre = 1; torre <=5; torre++){
-        printf("cima\n");
+        printf("cima\t");
     }
 
-
+    printf("\n");
     //bispo
     printf("\n");
     printf("##Bispo##\n");
 
     do
     {
-        printf ("cima, direita\n");
+        printf ("cima, direita\t");
         bispo++;
      } while (bispo <=5);
     
-
+     printf("\n");
 //rainha
      printf("\n");
      printf("##rainha##\n");
 
     while (rainha <= 8)
     {
-        printf("esquerda\n");
+        printf("esquerda\t");
         rainha++;
     }
     
-
+ printf("---------------------------------------------------------\n");
 
 
 
